b52d5606e303ef48bcbb42acabb0e06f0477c817
roshiniemmadi <97740417+roshiniemmadi@users.noreply.github.com>
2022-01-25 16:10:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello, welcome to ECS container application');
+  res.send('Hello, welcome to ECS container applications');
 });
 
 app.listen(PORT, HOST);
